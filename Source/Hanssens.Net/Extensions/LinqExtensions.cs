f0fdebddcf47cd41e0232af69761df028aa5ca32
Juliën Hanssens <j@hanssens.com>
2013-06-20 11:23:03 +0200

    LinqExtesion "In<T>" added
    - This to provide a simple, WHERE IN clause

---
+++ Source/Hanssens.Net/Extensions/LinqExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace System.Linq
+{
+    /// <summary>
+    /// Provides extensions for the System.Linq namespace.
+    /// </summary>
+    public static class LinqExtensions
+    {
+        /// <summary>
+        /// Filters a collection by a list of parameters, equivalant to a 'WHERE IN' clause.
+        /// </summary>
+        /// <typeparam name="T">TypeOf IEnumerable source</typeparam>
+        /// <param name="source">The source object</param>
+        /// <param name="list">List to filter by</param>
+        public static bool In<T>(this T source, params T[] list)
+        {
+            return list.Contains(source);
+        }
+    }
+}
