1f635d8e67b38964a125fe612d0136099805b87b
hanssens <github@hanssens.com>
2014-09-11 09:23:50 +0200

    Added .ToSeoFriendlyString() helper
    - Transform the input string into a dashed URL-friendly string
    - Tests provided, with one failure due to 'underscores'

---
+++ Source/Hanssens.Net.Tests/StringExtensionTests/ToSeoFriendlyStringTests.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using FluentAssertions;
+using Hanssens.Net.Extensions;
+using NUnit.Framework;
+
+namespace Hanssens.Net.Tests.StringExtensionTests
+{
+    [TestFixture]
+    public class ToSeoFriendlyStringTests
+    {
+
+        [Test]
+		public void ToSeoFriendlyString_Should_Strip_All_NonAlphaNumeric_Characters()
+        {
+			// arrange
+            var sentence = "This: is something that should be !@#$ _ clean";
+
+			// act
+			var target = sentence.ToSeoFriendlyString ();
+
+			// assert
+			target.Should ().NotBeNullOrEmpty ();
+            target.Should().BeEquivalentTo("this-is-something-that-should-be-clean");
+
+			foreach (var c in target.ToCharArray()) {
+				// allow 'dashes'
+				if (Char.Equals(c, '-'))
+					continue;
+
+				// iterate through the characters to prove that it's a letter, digit or seperator
+				if (!Char.IsLetterOrDigit (c))
+					Assert.Fail (String.Format ("Non-compliant character '{0}' found in pharse '{1}'", c.ToString (), target));
+			}
+        }
+
+		[Test]
+		public void ToSeoFriendlyString_Should_Only_Return_Lowercase_Characters()
+		{
+			// arrange
+			var sentence = "This SHOULD All Be In Lowercase";
+
+			// act
+			var target = sentence.ToSeoFriendlyString ();
+
+			// assert
+			target.Should ().NotBeNullOrEmpty ();
+			target.Should().BeEquivalentTo("this-should-all-be-in-lowercase");
+
+			foreach (var c in target.ToCharArray()) {
+				// skip any character that isn't a letter or digit
+				if (!Char.IsLetterOrDigit (c)) 
+					continue;
+
+				// iterate through the characters to prove that it only contains lowercase characters
+				if (!Char.IsLower(c))
+					Assert.Fail (String.Format ("Non-lowercase character '{0}' found in pharse '{1}'", c.ToString (), target));
+			}
+
+		}
+    }
+}
---
+++ Source/Hanssens.Net/Extensions/StringExtensions/ToSeoFriendlyString.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Hanssens.Net
+{
+	public static class ToSeoFriendlyStringExtensions
+	{
+		/// <summary>
+		/// Returns a lowercase copy of the string, with non-compliant characters for a url stripped out.
+		/// </summary>
+		/// <returns>The seo friendly string.</returns>
+		public static string ToSeoFriendlyString(this string input)
+		{
+			return Regex.Replace(input, @"[^A-Za-z0-9_\.~]+", "-")
+				.ToLowerInvariant();
+		}
+	}
+}
+
