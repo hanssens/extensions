739a616fc2f00ba8f45baeee767ec3bb18c41ec2
hanssens <github@hanssens.com>
2014-12-24 13:54:41 +0100

    Added function: Extract.ExtractPropertyValue
    - Helper for extracting the property value from an object instance, by it's property name

---
+++ Source/Hanssens.Net.Tests/Entities/CustomerWithLowerCaseProperties.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Hanssens.Net.Tests
+{
+	public class CustomerWithLowerCaseProperties
+	{
+		public int id { get; set; }
+		public string name { get; set; }
+
+	}
+}
+
---
+++ source/Hanssens.Net.Tests/Entities/Customer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Hanssens.Net.Tests.Entities
+{
+	public class Customer
+	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+
+	}
+}
+
