a0040f783c4e5723518254b8b4ef68e57284f986
hanssens <github@hanssens.com>
2015-09-02 16:15:50 +0200

    Updated version to v1.2.0

--- source/Hanssens.Net/Properties/AssemblyInfo.cs
+++ source/Hanssens.Net/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Juliën Hanssens @hanssens.com")]
 [assembly: AssemblyProduct("Hanssens.Net")]
-[assembly: AssemblyCopyright("Copyright © Juliën Hanssens @hanssens.com 2013")]
+[assembly: AssemblyCopyright("Copyright © Juliën Hanssens @hanssens.com 2013-2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1.1")]
-[assembly: AssemblyFileVersion("1.1.1")]
+[assembly: AssemblyVersion("1.2.0")]
+[assembly: AssemblyFileVersion("1.2.0")]
