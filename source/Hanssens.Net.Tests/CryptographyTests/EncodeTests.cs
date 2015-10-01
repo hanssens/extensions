b4766e3dbd1de6e46b4b7a4ec7c6c6ad39140037
hanssens <github@hanssens.com>
2015-10-01 16:15:35 +0200

    Basic setup of base64 encoding/decoding helpers

---
+++ source/Hanssens.Net.Tests/CryptographyTests/EncodeTests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Hanssens.Net.Cryptography;
+using NUnit.Framework;
+
+namespace Hanssens.Net.Tests.CryptographyTests
+{
+    [TestFixture]
+    public class EncodeTests
+    {
+        [Test]
+        public void Encode_Base64_Simple_String()
+        {
+            // arrange
+            var input = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed molestie lacus condimentum, viverra metus cursus, varius elit. Vestibulum pellentesque nibh non justo aliquam dignissim. Mauris auctor ipsum quam, ut cursus ipsum pharetra quis. Donec non nisi dapibus, sollicitudin libero at, pretium massa.";
+
+            // act
+            var target = Encode.Base64Encode(input);
+
+            // assert
+            target.Should().NotBeNullOrEmpty();
+            target.Should().NotBe(input);
+        }
+
+        [Test]
+        public void Decode_Base64_Simple_String()
+        {
+            // arrange - encode an original string
+            var original = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed molestie lacus condimentum, viverra metus cursus, varius elit. Vestibulum pellentesque nibh non justo aliquam dignissim. Mauris auctor ipsum quam, ut cursus ipsum pharetra quis. Donec non nisi dapibus, sollicitudin libero at, pretium massa.";
+            var encoded = Encode.Base64Encode(original);
+
+            // act - decode it
+            var decoded = Encode.Base64Decode(encoded);
+
+            // assert
+            decoded.Should().NotBeNullOrEmpty();
+            decoded.Should().NotBe(encoded);
+            decoded.Should().Be(original);
+        }
+    }
+}
---
+++ source/Hanssens.Net/Cryptography/Encode.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hanssens.Net.Cryptography
+{
+    public static class Encode
+    {
+        /// <summary>
+        /// Encodes a string using base64.
+        /// </summary>
+        /// <param name="plainText"></param>
+        /// <returns>Returns a base64 encoded string.</returns>
+        /// <remarks>Thanks to http://stackoverflow.com/a/11743162/1039247 </remarks>
+        public static string Base64Encode(string plainText)
+        {
+            if (string.IsNullOrEmpty(plainText)) throw new ArgumentNullException(nameof(plainText));
+
+            var plainTextBytes = System.Text.Encoding.UTF8.GetBytes(plainText);
+            return System.Convert.ToBase64String(plainTextBytes);
+        }
+
+        /// <summary>
+        /// Decodes a base64 string to 'plain text'.
+        /// </summary>
+        /// <param name="base64EncodedData"></param>
+        /// <returns>Returns the base64 decoded 'plain text'.</returns>
+        /// <remarks>Thanks to http://stackoverflow.com/a/11743162/1039247 </remarks>
+        public static string Base64Decode(string base64EncodedData)
+        {
+            if (string.IsNullOrEmpty(base64EncodedData)) throw new ArgumentNullException(nameof(base64EncodedData));
+
+            var base64EncodedBytes = System.Convert.FromBase64String(base64EncodedData);
+            return System.Text.Encoding.UTF8.GetString(base64EncodedBytes);
+        }
+    }
+}
