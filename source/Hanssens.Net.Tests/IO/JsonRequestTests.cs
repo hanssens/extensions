f654cb1c06d4f88f52d8aab2a9a0687e494dc303
hanssens <github@hanssens.com>
2016-01-07 00:38:02 +0100

    Fixed tests for JsonRequest
    - Found a new public, fake service that's ideal for this integration test
    - PUT + DELETE are now also implemented

--- source/Hanssens.Net.Tests/IO/JsonRequestTests.cs
+++ source/Hanssens.Net.Tests/IO/JsonRequestTests.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Net;
 using FluentAssertions;
 using NUnit.Framework;
 using Hanssens.Net.IO;
@@ -10,24 +11,34 @@ namespace Hanssens.Net.Tests.IO
 	{
 
 		// TODO: The tests below shouldn't call an external service, obviously, but should
-		// be refactored to rely on a more appropriate 'embedded' api. This should also
-		// include writing tests for PUT and DELETE.
+		// be refactored to rely on a more appropriate 'embedded' api.
 
 		[Test]
 		public void JsonRequest_Delete_Should_Return_Json_String(){
-			Assert.Inconclusive ("TODO: Implement test for JsonRequest.Delete");
+			// arrange
+			var endpoint = @"http://jsonplaceholder.typicode.com/posts/1";
+
+			// act
+			var target = JsonRequest.Delete (endpoint);
+
+			// assert
+			target.Should ().NotBeNull ();
+			target.StatusCode.Should ().Be (HttpStatusCode.OK);
+			target.Success.Should ().BeTrue (because: target.ErrorMessage);
+			target.ErrorMessage.Should ().BeNull ();
 		}
 
 		[Test]
 		public void JsonRequest_Get_Should_Return_Json_String(){
 			// arrange
-			var endpoint = @"http://ip.jsontest.com/";
+			var endpoint = @"http://jsonplaceholder.typicode.com/posts";
 
 			// act
 			var target = JsonRequest.Get (endpoint);
 
 			// assert
 			target.Should ().NotBeNull ();
+			target.StatusCode.Should ().Be (HttpStatusCode.OK);
 			target.Success.Should ().BeTrue (because: target.ErrorMessage);
 			target.Value.Should ().NotBeNullOrEmpty ();
 			target.ErrorMessage.Should ().BeNull ();
@@ -36,13 +47,15 @@ namespace Hanssens.Net.Tests.IO
 		[Test]
 		public void JsonRequest_Post_Should_Return_Json_String(){
 			// arrange
-			var endpoint = @"http://ip.jsontest.com/";
+			var endpoint = @"http://jsonplaceholder.typicode.com/posts";
+			var body = new { foo = "Foo", bar = "Bar" };
 
 			// act
-			var target = JsonRequest.Post (endpoint);
+			var target = JsonRequest.Post (endpoint, args: body);
 
 			// assert
 			target.Should ().NotBeNull ();
+			target.StatusCode.Should ().Be (HttpStatusCode.Created);
 			target.Success.Should ().BeTrue (because: target.ErrorMessage);
 			target.Value.Should ().NotBeNullOrEmpty ();
 			target.ErrorMessage.Should ().BeNull ();
@@ -50,7 +63,19 @@ namespace Hanssens.Net.Tests.IO
 
 		[Test]
 		public void JsonRequest_Put_Should_Return_Json_String(){
-			Assert.Inconclusive ("TODO: Implement test for JsonRequest.Put");
+			// arrange
+			var endpoint = @"http://jsonplaceholder.typicode.com/posts/1";
+			var body = new { foo = "Foo", bar = "Bar" };
+
+			// act
+			var target = JsonRequest.Put (endpoint, args: body);
+
+			// assert
+			target.Should ().NotBeNull ();
+			target.StatusCode.Should ().Be (HttpStatusCode.OK);
+			target.Success.Should ().BeTrue (because: target.ErrorMessage);
+			target.Value.Should ().NotBeNullOrEmpty ();
+			target.ErrorMessage.Should ().BeNull ();
 		}
 			
 	}
