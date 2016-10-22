6f534f7e59fa05bb009ce81f0c3a2a6cf8fe7a94
hanssens <github@hanssens.com>
2016-10-22 11:17:29 +0200

    Initial, clean setup for dotnet-core structure

---
+++ src/Hanssens.Net/Foo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hanssens.Net
+{
+    public class Foo
+    {
+        public string Bar()
+        {
+            return "42";
+        }
+    }
+}
