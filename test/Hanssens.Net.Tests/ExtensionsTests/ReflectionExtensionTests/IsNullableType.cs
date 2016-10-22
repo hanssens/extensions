baf1db8001f95375f13f4a7c7e7ccef18a40ff2a
hanssens <github@hanssens.com>
2016-10-22 12:57:05 +0200

    Added ReflectionExtensions.IsNullableType() - to determine if the type of an object instance is nullable (either value type or reference type)

---
+++ test/Hanssens.Net.Tests/ExtensionsTests/ReflectionExtensionTests/IsNullableType.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Hanssens.Net.Extensions;
+using NUnit.Framework;
+
+namespace Hanssens.Net.Tests.ExtensionsTests.ReflectionExtensionTests
+{
+    [TestFixture, Category("ReflectionExtensions/IsNullableType")]
+    public class IsNullableTypeTests
+    {
+        [Test]
+        public void IsNullableTypeShouldValidateNullType()
+        {
+            // arrange
+            object obj = null;
+
+            // act
+            var target = ReflectionExtensions.IsNullableType(obj);
+
+            // assert
+            target.Should().BeTrue();
+        }
+
+        [Test]
+        public void IsNullableTypeShouldValidateNullableReferenceType()
+        {
+            // arrange
+            int? obj = null;
+
+            // act
+            var target = ReflectionExtensions.IsNullableType(obj);
+
+            // assert
+            target.Should().BeTrue();
+        }
+
+        [Test]
+        public void IsNullableTypeShouldInvalidateNonNullableReferenceType()
+        {
+            // arrange
+            var obj = new DateTime();
+
+            // act
+            var target = ReflectionExtensions.IsNullableType(obj);
+
+            // assert
+            target.Should().BeFalse();
+        }
+    }
+}
