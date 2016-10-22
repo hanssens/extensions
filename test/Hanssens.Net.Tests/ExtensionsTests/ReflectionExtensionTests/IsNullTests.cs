35659350b372f6cac28276c0f94554f950906685
hanssens <github@hanssens.com>
2016-10-22 12:56:47 +0200

    Added ReflectionExtensions.IsNull() - to determine if the instance of an object is null (either value type or reference type)

---
+++ test/Hanssens.Net.Tests/ExtensionsTests/ReflectionExtensionTests/IsNullTests.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Hanssens.Net.Extensions;
+using NUnit.Framework;
+
+namespace Hanssens.Net.Tests.ExtensionsTests.ReflectionExtensionTests
+{
+    [TestFixture, Category("ReflectionExtensions/IsNull")]
+    public class IsNullTests
+    {
+        [Test]
+        public void IsNullShouldValidateNullObject()
+        {
+            // arrange
+            object obj = null;
+            
+            // act
+            var target = ReflectionExtensions.IsNull(obj);
+
+            // assert
+            target.Should().BeTrue();
+        }
+
+        [Test]
+        public void IsNullShouldValidateNullableReferenceType()
+        {
+            // arrange
+            int? obj = null;
+
+            // act
+            var target = ReflectionExtensions.IsNull(obj);
+
+            // assert
+            target.Should().BeTrue();
+        }
+
+        [Test]
+        public void IsNullShouldInvalidateNonNullableReferenceType()
+        {
+            // arrange
+            var obj = new DateTime();
+
+            // act
+            var target = ReflectionExtensions.IsNull(obj);
+
+            // assert
+            target.Should().BeFalse();
+        }
+    }
+}
